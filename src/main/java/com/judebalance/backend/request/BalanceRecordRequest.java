020ed027415f25eca5d8f116952480426dc43d87
kay9803 <163793492+kay9803@users.noreply.github.com>
2025-04-30 09:30:23 +0900

    Update BalanceRecordRequest.java

--- src/main/java/com/judebalance/backend/request/BalanceRecordRequest.java
+++ src/main/java/com/judebalance/backend/request/BalanceRecordRequest.java
@@ -1,1 +1,14 @@
+// src/main/java/com/judebalance/backend/request/BalanceRecordRequest.java
+package com.judebalance.backend.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BalanceRecordRequest {
+    private Integer balanceTime; // 초 단위 기록
+}
 
