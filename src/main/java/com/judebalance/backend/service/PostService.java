b8408f4036e4fc2fd12c323b92acf7fbf95fd9d8
김강현 <kanghyunkim@Kay-Kims-MacBook-Air.local>
2025-04-30 14:10:42 +0900

    Fix: Sort 클래스 import 추가

--- src/main/java/com/judebalance/backend/service/PostService.java
+++ src/main/java/com/judebalance/backend/service/PostService.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.data.domain.Sort;
+
 
 @Service
 @RequiredArgsConstructor
