ecf1a48d1724f1d7008095b9e0a76c37a83d24c9
kay9803 <163793492+kay9803@users.noreply.github.com>
2025-04-30 09:44:54 +0900

    Update BalanceRecord.java

--- src/main/java/com/judebalance/backend/domain/BalanceRecord.java
+++ src/main/java/com/judebalance/backend/domain/BalanceRecord.java
@@ -20,7 +20,7 @@ public class BalanceRecord {
     @JoinColumn(name = "user_id")
     private User user;  // 해당 기록의 사용자
 
-    private Integer duration;  // 균형 잡은 시간 (초)
+    private Integer balanceTime;  // 균형 잡은 시간 (초)
 
     private LocalDate date;  // 기록한 날짜
 }
